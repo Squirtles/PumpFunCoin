85ec6de4ba470705ab9b265c0024de7b19cbb186
Squirtles <squirtlesfx@gmail.com>
2025-01-08 07:07:37 -0500

    Update signup.js

--- netlify/functions/signup.js
+++ netlify/functions/signup.js
@@ -1,6 +1,6 @@
-const fetch = require("node-fetch");
+import fetch from "node-fetch";
 
-exports.handler = async (event) => {
+export const handler = async (event) => {
     try {
         if (event.httpMethod !== "POST") {
             return {
